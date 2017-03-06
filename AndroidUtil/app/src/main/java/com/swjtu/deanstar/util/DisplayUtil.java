a1081e293fa893885f03189bf3b3364dfe1c386e
杨海鹏 <1345803474@qq.com>
2017-03-06 10:56:02 +0800

    update DisplayUtil

---
+++ AndroidUtil/app/src/main/java/com/swjtu/deanstar/util/DisplayUtil.java
@@ -0,0 +1,81 @@
+package com.swjtu.deanstar.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Created by yhp5210 on 2017/3/6.
+ */
+
+public class DisplayUtil {
+
+    /**
+     * 将px值转换为dip或者dp值，保证尺寸大小不变
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     * @param context
+     * @param dipValue
+     * @return
+     */
+    public static int dip2px(Context context,float dipValue){
+
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dipValue * scale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context,float pxValue){
+
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int)(pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context,float spValue){
+
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int)(spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * dp2px
+     * @param dp
+     * @param context
+     * @return
+     */
+    protected int dp2px(int dp,Context context){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        dp,context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp2px
+     * @param sp
+     * @param context
+     * @return
+     */
+    protected int sp2px(int sp,Context context){
+
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                sp,context.getResources().getDisplayMetrics());
+    }
+}
